93ed505ffd5c7b81fd59b560e92263ce3d745541
Rebecca Haggard <beccahagg5@gmail.com>
2020-03-27 13:58:23 -0600

    layout done

--- screens/StatsScreen.js
+++ screens/StatsScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import Header from '../components/Header';
+import {BACKGROUND, DARK_NEUTRAL, MEDIUM_NEUTRAL} from "../assets/styles/COLORS";
+import {SCROLL_SCREEN_HEIGHT} from "../assets/styles/NUMBERS";
 
 const statTypes = ['', 'S', 'G', 'TO', 'TA', 'GB', 'F', 'FP'];
 const stats = [
@@ -89,54 +91,56 @@ export class StatsScreen extends React.Component {
         return (
             <View>
                 <Header title="Stats"/>
-                <View style={[styles.row, styles.rowOuter]}>
+                <View style={[styles.row, styles.rowOuter, styles.playerHeader]}>
                     <View style={styles.colEven}>
-                        <Text style={[styles.heading]}>
+                        <Text style={[styles.playerUnderHeading, styles.heading]}>
                             Player
                         </Text>
-                        <Text style={styles.paddedL4}>
+                        <Text style={styles.playerUnderHeading}>
                             {this.player.name}
                         </Text>
                     </View>
                 </View>
                 <View style={[styles.row, styles.rowOuter]}>
                     <View style={styles.colEven}>
-                        <Text style={[styles.heading]}>
+                        <Text style={[styles.heading, styles.playerText]}>
                             Age
                         </Text>
-                        <Text style={styles.paddedL4}>
+                        <Text style={[styles.playerText]}>
                             {this.player.age}
                         </Text>
                     </View>
                     <View style={styles.colEven}>
-                        <Text style={[styles.heading]}>
+                        <Text style={[styles.heading, styles.playerText]}>
                             Position
                         </Text>
-                        <Text style={styles.paddedL4}>
+                        <Text style={[styles.playerText]}>
                             {this.player.position}
                         </Text>
                     </View>
                 </View>
                 <View style={[styles.row, styles.rowOuter]}>
                     <View style={styles.colEven}>
-                        <Text style={[styles.heading]}>
+                        <Text style={[styles.heading, styles.playerText]}>
                             Year
                         </Text>
-                        <Text style={styles.paddedL4}>
+                        <Text style={[styles.playerText]}>
                             {this.player.year}
                         </Text>
                     </View>
                     <View style={styles.colEven}>
-                        <Text style={[styles.heading]}>
+                        <Text style={[styles.heading, styles.playerText]}>
                             Hometown
                         </Text>
-                        <Text style={styles.paddedL4}>
+                        <Text style={[styles.playerText]}>
                             {this.player.hometown}
                         </Text>
                     </View>
                 </View>
                 <View>
-                    {this.getGrid()}
+                    <ScrollView style={styles.screen}>
+                        {this.getGrid()}
+                    </ScrollView>
                 </View>
             </View>
         );
@@ -148,6 +152,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+    screen: {
+        maxHeight: 380,
+    },
     grid: {
       margin: 16,
         backgroundColor: '#fff'
@@ -172,5 +179,74 @@ const styles = StyleSheet.create({
     },
     cellText: {
       textAlign: 'center',
+    },
+    card: {
+        backgroundColor: '#000',
+        // marginLeft: 4
+    },
+    rowOuter: {
+        padding: 4,
+        backgroundColor: DARK_NEUTRAL
+    },
+    rowHeaderHead: {
+        backgroundColor: MEDIUM_NEUTRAL,
+        padding: 0,
+        paddingLeft: 12,
+        paddingRight: 12,
+        justifyContent: 'space-between'
+    },
+    rowHeaderBody: {
+        backgroundColor: DARK_NEUTRAL,
+        padding: 14,
+        paddingLeft: 8,
+        paddingRight: 8,
+    },
+    rowCol: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        padding: 8,
+    },
+    colEven: {
+        width: 180
+    },
+    col0: {
+        paddingLeft: 8,
+        width: 70,
+    },
+    col1: {
+        width: 180
+    },
+    col2: {
+        paddingRight: 8
+    },
+    cardBody: {
+        backgroundColor: BACKGROUND,
+        padding: 16,
+    },
+    boundingBox: {
+        paddingLeft: 24
+    },
+    paddedL: {
+        paddingLeft: 8
+    },
+    paddedL4: {
+        paddingLeft: 4
+    },
+    heading: {
+        // paddingBottom: 4,
+        fontWeight: 'bold',
+    },
+    playerUnderHeading: {
+        fontSize: 18,
+        textAlign: 'left',
+    },
+    playerText: {
+       fontSize: 16,
+        paddingLeft: 24
+    },
+    playerHeader: {
+      justifyContent: 'flex-start',
+        paddingLeft: 16
     }
+
 });
