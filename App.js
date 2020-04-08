b347ed2342e4dd009d87c9038900fe3816a7d90e
Rebecca Haggard <beccahagg5@gmail.com>
2020-04-07 21:25:31 -0600

    login screen

--- App.js
+++ App.js
@@ -9,6 +9,9 @@ import {StatsScreen} from "./screens/StatsScreen";
 import { Ionicons } from '@expo/vector-icons';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
+import { TextInput } from 'react-native';
+import { KeyboardAvoidingView } from 'react-native';
+
 
 import {
     ACCENT_COLOR,
@@ -19,6 +22,9 @@ import {
     MAIN_COLOR,
     MEDIUM_GRAY
 } from "./assets/styles/COLORS";
+import {FONT_BOLD, FONT_MAIN, FONT_SUB, SCREEN_HEIGHT, SCREEN_WIDTH} from "./assets/styles/NUMBERS";
+import Button from "react-native-material-ui/src/Button";
+import {TouchableWithoutFeedback} from "react-native-web";
 
 const Tab = createBottomTabNavigator();
 
@@ -35,9 +41,10 @@ export default function App() {
 }
 
 export class Main extends React.Component{
+
     constructor(props) {
         super(props);
-        this.state = {fontsLoaded: false};
+        this.state = {fontsLoaded: false, signIn: false, signedIn: false, teamName: '', password: ''};
     }
 
     async _loadFontsAsync() {
@@ -49,48 +56,146 @@ export class Main extends React.Component{
         this._loadFontsAsync();
     }
 
+    setSignIn() {
+        this.setState({signIn: true});
+    }
+
+    setSignedIn() {
+        this.setState({signedIn: true});
+    }
+
+    setTeamName(teamName) {
+        this.setState({teamName: teamName});
+    }
+
+    setPassword(password) {
+        this.setState({password: password});
+    }
+
+    getSignIn() {
+            return (
+                <KeyboardAvoidingView behavior="padding" keyboardVerticalOffset={87} enabled>
+                    <View style={styles.titleView}>
+                        <Text style={[styles.title, {color: ACCENT_COLOR}]}>Jocks</Text>
+                    </View>
+                   <View style={styles.signInView}>
+                       <View>
+                            <Text style={styles.inputHeading}>
+                                Team Name
+                            </Text>
+                            <TextInput
+                                style={styles.inputText}
+                                onChangeText={text => this.setTeamName(text)}
+                                value={this.state.teamName}
+                            />
+                        </View>
+                        <View>
+                            <Text style={styles.inputHeading}>
+                                Password
+                            </Text>
+                            <TextInput
+                                secureTextEntry={true}
+                                style={styles.inputText}
+                                onChangeText={text => this.setPassword(text)}
+                                value={this.state.password}
+                            />
+                        </View>
+                        <View style={styles.buttonContainer}>
+                            <TouchableWithoutFeedback onPress={(event) => this.setSignedIn()}>
+                                <View style={styles.button}>
+                                    <Text style={styles.buttonText}>
+                                        Sign In
+                                    </Text>
+                                </View>
+                            </TouchableWithoutFeedback>
+                        </View>
+                    </View>
+                </KeyboardAvoidingView>
+            );
+    }
+
     render() {
         if (this.state.fontsLoaded) {
-            return (
-                <View style={styles.container}>
-                    <NavigationContainer style={styles.screen}>
-                        <Tab.Navigator
-                            screenOptions={({route}) => ({
-                                tabBarIcon: ({focused, color, size}) => {
-                                    let iconName;
+            if (this.state.signedIn) {
+                return (
+                    <View style={styles.container}>
+                        <NavigationContainer style={styles.screen}>
+                            <Tab.Navigator
+                                screenOptions={({route}) => ({
+                                    tabBarIcon: ({focused, color, size}) => {
+                                        let iconName;
 
-                                    if (route.name === 'Home') {
-                                        iconName = focused ? 'ios-home' : 'ios-home';
-                                    } else if (route.name === 'Schedule') {
-                                        iconName = focused ? 'ios-calendar' : 'ios-calendar';
-                                    } else if (route.name === 'Report') {
-                                        iconName = focused ? 'ios-checkbox' : 'ios-checkbox';
-                                    } else if (route.name === 'Stats') {
-                                        iconName = focused ? 'ios-stats' : 'ios-stats';
+                                        if (route.name === 'Home') {
+                                            iconName = focused ? 'ios-home' : 'ios-home';
+                                        } else if (route.name === 'Schedule') {
+                                            iconName = focused ? 'ios-calendar' : 'ios-calendar';
+                                        } else if (route.name === 'Report') {
+                                            iconName = focused ? 'ios-checkbox' : 'ios-checkbox';
+                                        } else if (route.name === 'Stats') {
+                                            iconName = focused ? 'ios-stats' : 'ios-stats';
+                                        }
+
+                                        // You can return any component that you like here!
+                                        return <Ionicons name={iconName} size={size} color={color}/>;
+                                    },
+                                })}
+                                tabBarOptions={{
+                                    activeTintColor: ACCENT_COLOR, //'#39ff00',
+                                    inactiveTintColor: 'gray',
+                                    style: {
+                                        backgroundColor: LIGHT_GRAY,
+                                        borderTopColor: MEDIUM_GRAY,
+                                        borderTopWidth: 1
                                     }
-
-                                    // You can return any component that you like here!
-                                    return <Ionicons name={iconName} size={size} color={color}/>;
-                                },
-                            })}
-                            tabBarOptions={{
-                                activeTintColor: ACCENT_COLOR, //'#39ff00',
-                                inactiveTintColor: 'gray',
-                                style: {
-                                    backgroundColor: LIGHT_GRAY,
-                                    borderTopColor: MEDIUM_GRAY,
-                                    borderTopWidth: 1
-                                }
-                            }}
-                        >
-                            <Tab.Screen name="Home" component={HomeScreen}/>
-                            <Tab.Screen name="Schedule" component={ScheduleScreen}/>
-                            <Tab.Screen name="Report" component={ReportScreen}/>
-                            <Tab.Screen name="Stats" component={StatsScreen}/>
-                        </Tab.Navigator>
-                    </NavigationContainer>
-                </View>
-            );
+                                }}
+                            >
+                                <Tab.Screen name="Home" component={HomeScreen}/>
+                                <Tab.Screen name="Schedule" component={ScheduleScreen}/>
+                                <Tab.Screen name="Report" component={ReportScreen}/>
+                                <Tab.Screen name="Stats" component={StatsScreen}/>
+                            </Tab.Navigator>
+                        </NavigationContainer>
+                    </View>
+                );
+            }
+            else if (this.state.signIn) {
+                return (
+                    <View style={styles.container}>
+                        <View style={[styles.screen, {backgroundColor: '#fff'}]}>
+                            {this.getSignIn()}
+                        </View>
+                    </View>
+                );
+            }
+            else {
+                return (
+                    <View style={styles.container}>
+                        <View style={[styles.screen, {backgroundColor: '#fff'}]}>
+                            <View>
+                                <View style={styles.titleView}>
+                                    <Text style={[styles.title, {color: ACCENT_COLOR}]}>Jocks</Text>
+                                </View>
+                                <View style={styles.subHeadingView}>
+                                    <Text style={styles.subHeading}>
+                                        For Communication Off The Field
+                                    </Text>
+                                </View>
+                                <View style={styles.buttonContainer}>
+                                    <TouchableWithoutFeedback onPress={(event) => this.setSignIn()}>
+                                        <View>
+                                            <View style={[styles.button, {alignSelf: 'center', backgroundColor: ACCENT_COLOR}]}>
+                                                <Text style={[styles.buttonText, {color: '#fff'}]}>
+                                                    Sign In
+                                                </Text>
+                                            </View>
+                                        </View>
+                                    </TouchableWithoutFeedback>
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                );
+            }
         }
         else {
             return <AppLoading />;
@@ -101,6 +206,81 @@ export class Main extends React.Component{
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: '#fff',
     },
+    screen: {
+        backgroundColor: '#fff',
+        height: SCREEN_HEIGHT,
+        display: 'flex',
+        justifyContent: 'center'
+    },
+    titleView: {
+        alignSelf: 'center',
+        // margin: 20
+    },
+    title: {
+        fontSize: 70,
+        fontFamily: FONT_MAIN,
+        color: ACCENT_COLOR
+    },
+    subHeadingView: {
+        alignSelf: 'center',
+        width: SCREEN_WIDTH,
+        // margin: 20,
+        // backgroundColor: LIGHT_GRAY,
+        padding: 16,
+    },
+    subHeading: {
+        fontSize: 20,
+        fontFamily: FONT_MAIN,
+        textAlign: 'center',
+        // color: DARK_GRAY
+    },
+    buttonText: {
+        fontFamily: FONT_BOLD,
+        fontSize: 20,
+        color: '#fff',
+        textAlign: 'center'
+    },
+    button: {
+        backgroundColor: ACCENT_COLOR,
+        // borderColor: DARK_GRAY,
+        // borderWidth: 1,
+        padding: 16,
+        paddingTop: 8,
+        paddingBottom: 10,
+        marginTop: 8
+    },
+    buttonContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        marginTop: 16
+    },
+    inputText: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        paddingBottom: 8,
+        backgroundColor: '#fff',
+        marginBottom: 4
+    },
+    inputHeading: {
+        textAlign: 'left',
+        fontFamily: FONT_SUB,
+        paddingTop: 8,
+        paddingBottom: 8,
+        fontSize: 16,
+        // marginBottom: 4
+    },
+    signInView: {
+        margin: 20,
+        marginLeft: 30,
+        marginRight: 30,
+        backgroundColor: LIGHT_GRAY,
+        padding: 16,
+        borderWidth: 2,
+        borderColor: MEDIUM_GRAY
+    }
 
 });
